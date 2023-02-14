9bb47709979e1219b84b3491dea3353a0bc47f08
BogartDME <edwin.pietrowski@gmail.com>
2023-02-13 20:53:30 -0500

    added comments model and Post routes

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const path = require("path");
+const { User, Posts, Comments } = require("../../models"); // not sure if we are creating a model for likes??
+const withAuth = require('../../utils/auth');
+
+
+
+
+
+// get all posts for dashboard
+
+router.get('/', withAuth, (req, res) => {
+  try{  
+  const postInfo = Posts.findAll({
+      // attributes: [
+      //   'id',
+      //   'post_title',
+      //   'post_text',
+      //   'likes',
+      //   'user_id',
+      //   'user_name',
+      //   [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'likes']
+      // ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+          include: {
+            model: User,
+            attributes: ['user_name']
+          }
+        },
+        {
+          model: User,
+          attributes: ['user_name']
+        },
+      ],
+    });
+        const posts = postInfo.map(post => post.get({ plain: true }));
+  
+        res.render('posts', {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
+    } catch(err) {
+        console.log(err);
+        res.status(500).json(err);
+      }
+  });
+
+
+  // post create
+  router.post('/', withAuth, async (req, res) => {
+    try {
+      const newPost = await Posts.create({
+        ...req.body,
+        user_id: req.session.userId,
+      });
+  
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
+
+  
+
+  //for deleting post
+  router.delete('/:id', withAuth, (req, res) => {
+    console.log('id', req.params.id);
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostInfo => {
+        if (!dbPostInfo) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostInfo);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  
+
+
+module.exports = router;
---
+++ models/Comments.js
@@ -0,0 +1,54 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "posts",
+        key: "id"
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true,
+      references: {
+        model: "user",
+        key: "id"
+      }
+    }
+  },
+  
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comments",
+  }
+);
+
+module.exports = Comments;
